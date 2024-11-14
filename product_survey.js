1ac6b9cf1cdfa6b87ee7e22a9c721099adaff635
Oladipupo Adenowun <oladipupo.adenowun@yahoo.com>
2024-11-13 20:50:12 -0500

    implemented the feedback feature

---
+++ product_survey.js
@@ -0,0 +1,34 @@
+function submitFeedback(){
+
+    alert("Thank you for your valuable feedback!");
+
+    /* read input from dom */
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const email = document.getElementById("email").value;
+    const job = document.getElementById("job").value;
+    const designation = document.getElementById("designation").value;
+    const productType = document.getElementById("productType").value;
+    const feedbackText = document.getElementById("feedbackText").value;
+
+    /*place the values in the information panel*/
+    document.getElementById("userName").innerHTML = name;
+    document.getElementById("userAge").innerHTML = age;
+    document.getElementById("userEmail").innerHTML = email;
+    document.getElementById("userJob").innerHTML = job;
+    document.getElementById("userDesignation").innerHTML = designation;
+    document.getElementById("userProductChoice").innerHTML = productType;
+    document.getElementById("userFeedback").innerHTML = feedbackText;
+
+    /*make information panel active*/
+    document.getElementById("userInfo").style.display = 'block';
+}
+
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener('submit',()=>submitFeedback());
+/*also process when enter button is pressed*/
+document.addEventListener('keydown',function(event){
+    if(event.key == 'Enter'){
+        submitFeedback();
+    }
+});
